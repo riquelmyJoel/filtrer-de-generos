be49af0c659327ca5d8ccbefe239d813e23f1368
riquelmyJoel <jhonnyteoficial@gmail.com>
2026-07-15 23:20:42 -0300

    colocando os códigos do arquivo pessoa.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+
+            System.out.print("Idade: ");
+            int idade = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Gênero (Masculino/Feminino): ");
+            String genero = sc.nextLine();
+
+            pessoas.add(new Pessoa(nome, idade, genero));
+
+            System.out.print("Deseja continuar? (S/N): ");
+            String resp = sc.nextLine();
+
+            if (resp.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        // Lambda para filtrar somente mulheres
+        List<Pessoa> mulheres = pessoas.stream()
+                .filter(p -> p.getGenero().equalsIgnoreCase("Feminino"))
+                .collect(Collectors.toList());
+
+        System.out.println("\nLista de Mulheres:");
+        mulheres.forEach(System.out::println);
+
+        sc.close();
+    }
+
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String genero;
+
+    public Pessoa(String nome, int idade, String genero) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + idade + " anos - " + genero;
+    }
 }
